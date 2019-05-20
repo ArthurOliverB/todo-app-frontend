727b1e1246cb86b933bac2f1de0c5d94516c3f2b
Arthur Oliveira <arthuroliveirabandeira@hotmail.com>
2019-05-20 13:49:12 -0300

    first commit

---
+++ build/precache-manifest.b5e3762b5da17e3d3d0110b3add26453.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d51f227f521e4771bdc6219cffbf677a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51926043044fa73068b8",
+    "url": "/static/css/2.49d5e55d.chunk.css"
+  },
+  {
+    "revision": "b727d8a8edac87bec392",
+    "url": "/static/css/main.08f8fdbb.chunk.css"
+  },
+  {
+    "revision": "51926043044fa73068b8",
+    "url": "/static/js/2.7027a3b2.chunk.js"
+  },
+  {
+    "revision": "b727d8a8edac87bec392",
+    "url": "/static/js/main.fc2d3cfc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "813c1b09f451054ce7404d2bc23c780e",
+    "url": "/static/media/plus.813c1b09.png"
+  }
+]);
